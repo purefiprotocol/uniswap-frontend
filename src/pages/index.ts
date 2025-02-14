01d322236fdf42eef8ce86e679595a51b5b5a25b
Mikhail Radchenko <mradchenko@purefi.io>
2025-02-14 13:43:50 +0300

    fix: css related to mobile

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,5 @@
 export { Home } from './Home';
 export { Liquidity } from './Liquidity';
+export { Positions } from './Positions';
 export { Kyc } from './Kyc';
 export { NotFound } from './NotFound';
