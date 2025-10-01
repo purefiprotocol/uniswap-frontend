ff6d4c33d69cfb785ba861e3c50d6a80970e1e62
Mikhail Radchenko <mradchenko@purefi.io>
2025-10-01 12:11:50 +0300

    chore: add debounce

--- src/utils/utility.ts
+++ src/utils/utility.ts
@@ -14,3 +14,19 @@ function throttle(func: Function, delay: number): Function {
     }, delay);
   };
 }
+
+function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeout: ReturnType<typeof setTimeout> | null = null;
+
+  return function (...args: Parameters<T>): void {
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+}
